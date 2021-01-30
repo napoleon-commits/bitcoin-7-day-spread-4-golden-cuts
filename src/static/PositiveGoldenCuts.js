0ad8de089052991b39d3551d8c26600817445ece
Byron Hall <byronhall@MacBook-Pro.local>
2021-01-29 22:34:09 -0600

    added base with GoldenCuts

---
+++ src/static/PositiveGoldenCuts.js
@@ -0,0 +1,11 @@
+export default (()=>{
+    const goldenRatio = ((1+(5**(1/2)))/2) - 1;
+    const cuts = [goldenRatio];
+    for(let i = 0; i < 8; i+=1){
+        cuts.push(
+            (1-(cuts.reduce((a,b)=>a+b)))*goldenRatio
+        );
+    }
+    cuts.push(1-cuts.reduce((a,b)=>a+b));
+    return cuts;
+})()
